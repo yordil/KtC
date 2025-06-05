301f59abd5eabefa0a98a72875c44ae587e5ac07
yordanosl <yordanoslegesse15@gmail.com>
2025-06-05 17:18:29 +0300

    initial commit

--- model/tasks.go
+++ model/tasks.go
@@ -1,15 +1,2 @@
 package model
 
-import (
-	"time"
-)
-
-type Task struct {
- ID          string    `json:"id"`
- Title       string    `json:"title" binding:"required"`
- Description string    `json:"description" binding:"required"`
- DueDate     time.Time `json:"due_date" binding:"required"`
- Status      string    `json:"status" binding:"required"`
-}
-
-
